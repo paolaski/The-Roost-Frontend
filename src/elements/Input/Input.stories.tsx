83ee3ab874dd5e120358b3f873862383d260a99d
Paula P. T <paulapereztafalla@gmail.com>
2021-12-14 17:44:33 +0100

    WIP - Some Components Created /w Manu Pair Programming

---
+++ src/elements/Input/Input.stories.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import Input from './Input';
+
+export default {
+  title: "Input",
+};
+
+export const Default = () => <Input />;
+
+Default.story = {
+  name: 'default',
+};
