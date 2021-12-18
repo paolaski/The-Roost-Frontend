c8b5c7d679be4c9077db1b93e36d9cfc5fb7882e
Manu Glez <manuaglez@gmail.com>
2021-12-18 12:47:40 +0000

    Shop view and product cards builded and displayed

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProductCard = (product: any) => (
+  <div className='productCard'>
+		<div className="productCard__image">
+			<img src={product.image} alt="placeholder" />
+		</div>
+		<div className="productCard__content">
+			<button>{product.name}</button>
+			<button>{product.type}</button>
+			<p className="productCard__text">{product.description}</p>
+			<button>Add to Cart {product.price} €</button>
+		</div>
+  </div>
+);
+
+export default ProductCard;
