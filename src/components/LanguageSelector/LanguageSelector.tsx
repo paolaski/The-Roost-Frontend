579fb75d251edd6d2790e4933bd85c9b501e25ce
Paula P. T <paulapereztafalla@gmail.com>
2021-12-18 17:13:17 +0100

    Added Lang-Selector as Submenu

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useState, useEffect, useRef, FC} from 'react';
-import { NavLink, Link} from 'react-router-dom';
+import React, { FC} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { RootState } from '../../store';
 import { setLanguage } from '../../store/langActions/langAction';
-import { translate } from '../../i18n';
+import { BsDot } from "react-icons/bs";
+import { RiTranslate } from "react-icons/ri";
+
 
 interface HeaderProps {
 	fixed?: boolean;
@@ -12,48 +12,22 @@ interface HeaderProps {
 }
 
 const LanguageSelector: FC<HeaderProps> = ({fixed, transparent}) => {
-	const {} = useSelector((state:RootState) => state.lang);
+	const { language } = useSelector((state:RootState) => state.lang);
 	const dispatch = useDispatch();
-	const [showDropdown, setShowDropdown ] = useState(false);
-	const dropdownEl = useRef<HTMLUListElement>(null);
-
-	let langSelectorClass = 'lang-selector';
-
-	if(fixed){
-		langSelectorClass += ' langSelector--fixed';
-	}
-
-	if(transparent){
-		langSelectorClass += ' langSelector--transparent';
-	}
-
-	const handleClickOutside = useCallBack((e) => {
-		if(showDropdown && e.target.closest('.dropdown') != dropdownEl.current){
-			setShowDropdown(false);
-		}
-	}, [showDropdown, setShowDropdown, dropdownEl]);
-	useEffect(() => {
-		document.addEventListener('click', handleClickOutside);
-		return () => {
-			document.removeEventListener('click', handleClickOutside);
-		}
-	}, [handleClickOutside]);
 
 	const chooseLanguageHandler = (value:string) => {
-		setShowDropdown(false);
 		dispatch(setLanguage(value));
 	}
 
 	return(
-		<LanguageSelector data-testid="LanguageSelector" className={langSelectorClass}>
-			<div className="nav-lang">
-				<p className="selected" onClick={() => setShowDropdown(true)}>{language}</p>
-				{showDropdown && <ul className="lang-dropdown" ref={dropdownEl}>
-					<li onClick={() => chooseLanguageHandler('EN')}>EN</li>
-					<li onClick={() => chooseLanguageHandler('ES')}>ES</li>
-				</ul>}
+		<>
+			<div  className="lang-selector">
+				<span className="lang-selector__lang-list">
+					<RiTranslate className="lang-selector__icon" />
+					<a className = "lang-selector__lang-list__item" onClick={() => chooseLanguageHandler('EN')}>EN</a><BsDot className="lang-selector__icon"/>				<a className = "lang-selector__lang-list__item"  onClick={() => chooseLanguageHandler('ES')}>ES</a>
+				</span>
 			</div>
-		</LanguageSelector>
+		</>
 	)
 }
 
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,11 +2,11 @@ import en from './en.json';
 import es from './es.json';
 
 export const translate = (key:string, language: string): string => {
-	let langData: { [key:string]: string } = {};
+	let langData: { [key: string]: string } = {};
 	if(language === 'EN'){
 		langData = en;
 	}else if(language === 'ES') {
-		langData = es
+		langData = es;
 	}
 
 	return langData[key];
