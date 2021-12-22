fa0c40b45ad85c201f97e6dcab70579de9f3c49c
Manu Glez <manuaglez@gmail.com>
2021-12-22 01:11:00 +0000

    base calendar

--- src/view/Reservations/Reservations.tsx
+++ src/view/Reservations/Reservations.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 
 
-const Reservations = () => (
-  <div data-testid="Reservations">
-    Reservations Component
-  </div>
-);
+const Reservation = () => {
+	const [value, onChange] = useState(new Date())
+	return (
 
-export default Reservations;
+		<>
+			<div className="reservationContainer">
+				<div className="reservationContainer__division">
+					<div className="reservationContainer__division__section">
+						<Calendar onChange={onChange} value={value} />
+
+					</div>
+				</div>
+				<div className="reservationContainer__division">
+					<label htmlFor="telInput">Phone number</label>
+					<input type="tel" id="telInput" />
+					<label htmlFor="clientesInput">How many</label>
+					<input type="number" id="clientesInput" />
+					<label htmlFor="commentsTxt">Observations</label>
+					<textarea name="" id="commentsTxt"></textarea>
+					<button>Button</button>
+				</div>
+			</div>
+		</>
+	);
+}
+export default Reservation;
